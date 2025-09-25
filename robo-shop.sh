b542be994035bede3f264b5b8ed119740ead91a0
venkat-veer <s.venkatveer@gmail.com>
2025-09-26 00:19:59 +0530

    shell-roboshop

--- robo-shop.sh
+++ robo-shop.sh
@@ -1,24 +1,24 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-AMI_ID="ami-09c813fb71547fc4f"          # ami -id same for all siva also same
-SG_ID="sg-0f80e4641e406c1e3"            # it is different siva id different mine is different.
-ZONE_ID="Z05359301W668SBI8XW8V"         # hosted-zone id of mine differ for every one.
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0f80e4641e406c1e3" # replace with your SG ID
+ZONE_ID="Z05359301W668SBI8XW8V" # replace with your ID
 DOMAIN_NAME="devaws.store"
 
-for instance in $@
-do                              # view word warp ok to get multi lines for below line
+for instance in $@ # mongodb redis mysql
+do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-    # get private IP
+    # Get Private IP
     if [ $instance != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)  
-        RECORD_NAME="$instance.$DOMAIN_NAME" #mongodb.devaws.store
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.devaws.store
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        RECORD_NAME="$DOMAIN_NAME"           #devaws.store
+        RECORD_NAME="$DOMAIN_NAME"           # devaws.store
     fi
 
-    echo "$instance:$IP"
+    echo "$instance: $IP"
 
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
