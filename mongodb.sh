37b1018320872cc933ce9d1dcd36838547f0153b
venkat-veer <s.venkatveer@gmail.com>
2025-09-26 12:22:00 +0530

    allow remote connections

--- mongodb.sh
+++ mongodb.sh
@@ -39,6 +39,12 @@ VALIDATE $? "Enable MongoDB"
 systemctl start mongod
 VALIDATE $? "Start MongoDB"
 
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to MongoDB"
+
+systemctl restart mongod
+VALIDATE $? "Restarted MongoDB"
+
 
 
 
