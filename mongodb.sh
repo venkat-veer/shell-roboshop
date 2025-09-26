85f3a9c32c9bc94725b753827ccc2e3b6de841a5
venkat-veer <s.venkatveer@gmail.com>
2025-09-26 15:35:09 +0530

    allow remote connections

--- mongodb.sh
+++ mongodb.sh
@@ -30,13 +30,29 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Adding Mongo repo"
 
+# dnf install mongodb-org -y &>>$LOG_FILE
+# VALIDATE $? "Installing MongoDB"
+
+# systemctl enable mongod &>>$LOG_FILE
+# VALIDATE $? "Enable MongoDB"
+
+# systemctl start mongod
+# VALIDATE $? "Start MongoDB"
+
+# sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+# VALIDATE $? "Allowing remote connections to MongoDB"
+
+# systemctl restart mongod
+# VALIDATE $? "Restarted MongoDB"
+
+
 dnf install mongodb-org -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB"
 
 systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "Enable MongoDB"
 
-systemctl start mongod
+systemctl start mongod 
 VALIDATE $? "Start MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
@@ -44,7 +60,5 @@ VALIDATE $? "Allowing remote connections to MongoDB"
 
 systemctl restart mongod
 VALIDATE $? "Restarted MongoDB"
-
-
 
 
