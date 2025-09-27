77824b5bffa98ebfb306c13db46528c549a0a725
venkat-veer <s.venkatveer@gmail.com>
2025-09-27 11:43:52 +0530

    user-script

---
+++ 7-user.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/bash
+
+# https://github.com/daws-86s/roboshop-documentation/blob/main/05-user.MD
+# https://github.com/daws-86s/shell-roboshop/blob/main/user.sh
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.devaws.store
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+##### NODEJS #######
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling NodeJS"
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enable NodeJS 20"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing NodeJS"
+
+#idempotency
+id roboshop &>>$LOG_FILE 
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE   
+    VALIDATE $? "Create System User"
+else
+    echo -e "User already Exists ... $Y SKIPPING $N"
+fi
+
+mkdir -p /app
+VALIDATE $? "Creating App Directory"
+
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading User Application"
+
+cd /app
+VALIDATE $? "Changing to app Directory"
+
+rm -rf /app/*
+VALIDATE $? "Remove Existing Code"
+
+unzip /tmp/user.zip &>>$LOG_FILE
+VALIDATE $? "Unzip User"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "Install Dependencies"
+
+cp $SCRIPT_DIR/7.1-user.service /etc/systemd/system/user.service
+VALIDATE $? "Copy systemctl service"
+
+systemctl daemon-reload
+systemctl enable user &>>$LOG_FILE
+VALIDATE $? "Enable user"
+
+systemctl restart user
+VALIDATE $? "Restart User"
+
+
