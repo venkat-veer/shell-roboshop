aeffdfb4ca6c2e32e7354c923558738347ccf35b
venkat-veer <s.venkatveer@gmail.com>
2025-09-27 08:59:56 +0530

    redis-script

--- 4-redis.sh
+++ 4-redis.sh
@@ -12,6 +12,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+START_TIME=$(date +%s)
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
