fd7aaaf191dd0590181196d33f4212d395d0345b
venkat-veer <s.venkatveer@gmail.com>
2025-09-27 15:33:52 +0530

    check-scripts

--- 4-redis.sh
+++ 4-redis.sh
@@ -13,7 +13,6 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
-
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
@@ -32,24 +31,20 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
 }
 
 dnf module disable redis -y &>>$LOG_FILE
-VALIDATE $? "Disable Default Redis"
-
-dnf module enable redis:7 -y&>>$LOG_FILE 
-VALIDATE $? "Enable Redis 7"
-
-dnf install redis -y &>>$LOG_FILE
+VALIDATE $? "Disabling Default Redis"
+dnf module enable redis:7 -y &>>$LOG_FILE
+VALIDATE $? "Enabling Redis 7"
+dnf install redis -y  &>>$LOG_FILE
 VALIDATE $? "Installing Redis"
 
-sed -i -e "s/127.0.0.1/0.0.0.0/g" -e "/protected-mode/ c protected-mode no" /etc/redis/redis.conf
-VALIDATE $? "Allow Remote Connections to Redis"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing Remote connections to Redis"
 
 systemctl enable redis &>>$LOG_FILE
-VALIDATE $? "Enable Redis"
-
+VALIDATE $? "Enabling Redis"
 systemctl start redis &>>$LOG_FILE
-VALIDATE $? "Start Redis"
-
+VALIDATE $? "Starting Redis"
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME-$START_TIME ))
-echo -e "Script Executed in: $Y $TOTAL_TIME Seconds $N"
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
--- 6-rabbitmq.sh
+++ 6-rabbitmq.sh
@@ -3,7 +3,7 @@
 # https://github.com/daws-86s/roboshop-documentation/blob/main/09-rabbitmq.MD
 # https://github.com/daws-86s/shell-roboshop/blob/main/rabbitmq.repo
 
-USERID=$(id -u)
+UUSERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -13,9 +13,8 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
-SCRIPT_DIR=$PWD
-
 mkdir -p $LOGS_FOLDER
+SCRIPT_DIR=$PWD
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
@@ -33,22 +32,17 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
 }
 
 cp $SCRIPT_DIR/6.1-rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
-VALIDATE $? "Adding RabbitMq repo"
-
-
+VALIDATE $? "Adding RabbitMQ repo"
 dnf install rabbitmq-server -y &>>$LOG_FILE
-VALIDATE $? "Installing Rabbitmq server"
-
+VALIDATE $? "Installing RabbitMQ Server"
 systemctl enable rabbitmq-server &>>$LOG_FILE
-VALIDATE $? "Enable RabbitMq server"
-
+VALIDATE $? "Enabling RabbitMQ Server"
 systemctl start rabbitmq-server &>>$LOG_FILE
-VALIDATE $? "Start  RabbitMq server"
-
+VALIDATE $? "Starting RabbitMQ"
 rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
-VALIDATE $? "Setting Permissions"
+VALIDATE $? "Setting up permissions"
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME-$START_TIME ))
-echo -e "Script Executed in: $Y $TOTAL_TIME Seconds $N"
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
